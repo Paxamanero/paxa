807f9b001870e50ba08478edf24687817fb7c4b2
Paxamanero <97613000+Paxamanero@users.noreply.github.com>
2023-08-05 16:19:05 +0300

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x8c99550181d80ba86e3f8668b0aa15bc0bdbaa2a";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
